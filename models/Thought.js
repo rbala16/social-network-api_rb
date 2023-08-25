45659b1b35b547400bcc4b36e9c544198dc78194
Rajni bala <balatamna16@gmail.com>
2023-08-24 22:26:24 -0400

    reaction schema created

--- models/Thought.js
+++ models/Thought.js
@@ -3,6 +3,36 @@ const { Schema, model ,Types} = require('mongoose');
 // import moment module to format the timestamp 
 const moment = require('moment');
 
+const ReactionSchema = new Schema({
+  reactionId: {
+      type: Types.ObjectId,
+      default: new Types.ObjectId()
+  },
+  reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+  },
+  username: {
+      type: String,
+      required: true
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+  }
+},
+{
+  toJSON: {
+    virtuals:true,
+      getters: true
+  },
+  id: false
+});
+
+
+//thought schema
 const ThoughtSchema = new Schema({
   thoughtText: {
     type: String,
@@ -19,7 +49,7 @@ username: {
   type: String,
   required: true
 },
-reactions: [reactionSchema]
+reactions: [ReactionSchema]
 },
 {
   toJSON: {
@@ -37,6 +67,6 @@ ThoughtSchema.virtual('reactionCount').get(function() {
 });
 
 const Thought = model('Thought', ThoughtSchema);
-
+// export the Thought model
 module.exports = Thought;
 
